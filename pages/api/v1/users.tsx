42f6cb9d2f0ceca6103d45fd47161417aa54b2c6
LEOCHEUNG <leozhang621@gmail.com>
2022-01-16 13:35:41 +0800

    refactor: 重构User的验证逻辑

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -1,5 +1,4 @@
 import { NextApiHandler } from 'next';
-import md5 from 'md5';
 
 import { getDatabaseConnection } from '../../../lib/getDatabaseConnection';
 import { User } from '../../../src/entity/User';
@@ -7,38 +6,16 @@ import { User } from '../../../src/entity/User';
 const Users: NextApiHandler = async (req, res) => {
   const connection = await getDatabaseConnection();
   const { username, password, passwordConfirmation } = req.body;
-  const errors = { username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[] };
   res.setHeader('Content-Type', 'application/json;charset=utf-8');
-  const foundUser = connection.manager.find(User, { username });
-  if (foundUser) {
-    errors.username.push('用户名已存在');
-  }
-  if (!username.trim()) {
-    errors.username.push('用户名不能为空');
-  }
-  if (!/[a-zA-Z0-9]/g.test(username.trim())) {
-    errors.username.push('用户名格式不合法');
-  }
-  if (username.trim().length > 42) {
-    errors.username.push('用户名太长');
-  }
-  if (username.trim().length < 3) {
-    errors.username.push('用户名太短');
-  }
-  if (password === '') {
-    errors.password.push('密码不能为空');
-  }
-  if (password !== passwordConfirmation) {
-    errors.passwordConfirmation.push('密码不匹配');
-  }
-  const hasError = Object.values(errors).find(error => error.length > 0);
-  if (hasError) {
+  const user = new User();
+  user.username = username.trim();
+  user.password = password;
+  user.passwordConfirmation = passwordConfirmation;
+  await user.validate();
+  if (user.hasError()) {
     res.statusCode = 422;
-    res.write(JSON.stringify(errors));
+    res.write(JSON.stringify(user.errors));
   } else {
-    const user = new User();
-    user.username = username.trim();
-    user.passwordDigest = md5(password);
     await connection.manager.save(user);
     res.statusCode = 200;
     res.write(JSON.stringify(user));
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,10 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import md5 from 'md5';
 
 import { Post } from './Post';
 import { Comment } from './Comment';
+
+import { getDatabaseConnection } from '../../lib/getDatabaseConnection';
 
 @Entity('users')
 export class User {
@@ -25,4 +28,43 @@ export class User {
 
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[];
+
+  errors: { username: string[], password: string[], passwordConfirmation: string[] } = {
+    username: [],
+    password: [],
+    passwordConfirmation: []
+  };
+  password: string;
+  passwordConfirmation: string;
+
+  async validate() {
+    const connection = await getDatabaseConnection();
+    const foundUser = await connection.manager.find(User, { username: this.username });
+    if (foundUser && foundUser.length > 0) {
+      console.log(foundUser);
+      this.errors.username.push('用户名已存在');
+    }
+    if (!this.username.trim()) {
+      this.errors.username.push('用户名不能为空');
+    }
+    if (!/[a-zA-Z0-9]/g.test(this.username.trim())) {
+      this.errors.username.push('用户名格式不合法');
+    }
+    if (this.username.trim().length > 42) {
+      this.errors.username.push('用户名太长');
+    }
+    if (this.username.trim().length < 3) {
+      this.errors.username.push('用户名太短');
+    }
+    if (this.password === '') {
+      this.errors.password.push('密码不能为空');
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('密码不匹配');
+    }
+  }
+
+  hasError(): boolean {
+    return !!Object.values(this.errors).find(error => error.length > 0);
+  }
 }
