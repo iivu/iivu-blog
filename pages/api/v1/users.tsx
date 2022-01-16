de62a9628e013be318d1f7e2de3bc0a4002bc33e
LEOCHEUNG <leozhang621@gmail.com>
2022-01-16 13:05:08 +0800

    feat: User的username字段添加唯一性检查和索引 feat: 添加用户名重复的验证逻辑

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -5,9 +5,14 @@ import { getDatabaseConnection } from '../../../lib/getDatabaseConnection';
 import { User } from '../../../src/entity/User';
 
 const Users: NextApiHandler = async (req, res) => {
+  const connection = await getDatabaseConnection();
   const { username, password, passwordConfirmation } = req.body;
   const errors = { username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[] };
   res.setHeader('Content-Type', 'application/json;charset=utf-8');
+  const foundUser = connection.manager.find(User, { username });
+  if (foundUser) {
+    errors.username.push('用户名已存在');
+  }
   if (!username.trim()) {
     errors.username.push('用户名不能为空');
   }
@@ -31,7 +36,6 @@ const Users: NextApiHandler = async (req, res) => {
     res.statusCode = 422;
     res.write(JSON.stringify(errors));
   } else {
-    const connection = await getDatabaseConnection();
     const user = new User();
     user.username = username.trim();
     user.passwordDigest = md5(password);
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,37 +1,31 @@
 import React from 'react';
-import {getPost, getPostIds} from '../../lib/posts';
-import {NextPage} from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+
+import { getDatabaseConnection } from '../../lib/getDatabaseConnection';
+import { Post } from '../../src/entity/Post';
 
 type Props = {
   post: Post
 }
-const postsShow: NextPage<Props> = (props) => {
-  const {post} = props;
+
+const PostsShow: NextPage<Props> = (props) => {
+  const { post } = props;
   return (
     <div>
       <h1>{post.title}</h1>
-      <article dangerouslySetInnerHTML={   {__html: post.htmlContent}  }>
+      <article dangerouslySetInnerHTML={{ __html: post.content }}>
       </article>
     </div>
   );
 };
 
-export default postsShow;
+export default PostsShow;
 
-export const getStaticPaths = async () => {
-  const idList = await getPostIds();
+export const getServerSideProps: GetServerSideProps<any, { id: string }> = async (context) => {
+  const { params: { id } } = context;
+  const connection = await getDatabaseConnection();
+  const post = await connection.manager.findOne(Post, id);
   return {
-    paths: idList.map(id => ({params: {id: id}})),
-    fallback: true
-  };
-};
-
-export const getStaticProps = async (x: any) => {
-  const id = x.params.id;
-  const post = await getPost(id);
-  return {
-    props: {
-      post: post
-    }
+    props: { post: JSON.parse(JSON.stringify(post)) }
   };
 };
